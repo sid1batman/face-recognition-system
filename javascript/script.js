9570950e7d1bd613de4760f235e96604dbea9991
sid1batman <142674196+sid1batman@users.noreply.github.com>
2023-10-15 18:03:50 +0530

    add javascript folder

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPreView: 1,
+  grabCursor:true,
+  loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
